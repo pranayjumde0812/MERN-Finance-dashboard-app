6d994bf6cabf141c3a2fd58cdf8337d6e66354ad
Pranay Jumde <pranay.jumde@qodequay.com>
2024-10-24 14:45:13 +0530

    MUI Configuration and Themes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,19 @@
+import { createTheme } from "@mui/material/styles";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+
 function App() {
+  const theme = useMemo(() => createTheme(themeSettings), []);
+
   return (
     <>
-      <div className="app"></div>
+      <div className="app">
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          Hii
+        </ThemeProvider>
+      </div>
     </>
   );
 }
